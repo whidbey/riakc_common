bc9e655420b7b8bee573dfec2195e5d858599752
David.Gao <david.alpha.fox@gmail.com>
2016-05-11 18:18:01 +0800

    advance crud

--- lib/simple_rest/actions/crud.ex
+++ lib/simple_rest/actions/crud.ex
@@ -17,7 +17,12 @@ defmodule RiakcCommon.SimpleRest.Actions.CRUD do
       require RiakcCommon.SimpleRest.Utils.ApiContext
       import RiakcCommon.SimpleRest.Actions.CRUD , only: [request: 1, response: 1]
       unquote(compile(actions, opts))
+
+      import RiakcCommon.SimpleRest.Actions.CRUD, only: [operation_schema: 1]
+      Module.register_attribute(__MODULE__, :riakc_operation_fields, accumulate: true)
+
     end
+
   end
 
   defp compile(actions, opts) do
@@ -36,21 +41,74 @@ defmodule RiakcCommon.SimpleRest.Actions.CRUD do
     acc
   end
 
-  defmacro response(module) do   
+  defmacro operation_schema([do: block]) do
     quote do
-      def __response__() do
-        unquote(module)
+      
+      try do
+        import RiakcCommon.SimpleRest.Actions.CRUD
+        unquote(block)
+      after
+        :ok
       end
-    end
+      fields = @riakc_operation_fields |> Enum.reverse
 
+      Module.eval_quoted __ENV__, [
+        RiakcCommon.SimpleRest.Actions.CRUD.__types__(fields)
+      ]
+    end
   end
 
-  defmacro request(module) do
+  defmacro request(operation, type \\ nil , opts \\ []) do
     quote do
-      def __request__() do
-        unquote(module)
-      end
+      RiakcCommon.SimpleRest.Actions.CRUD.__request__(__MODULE__, unquote(operation), unquote(type), unquote(opts))
     end
   end
+
+  defmacro response(operation, type \\ nil, opts \\ []) do
+    quote do
+      RiakcCommon.SimpleRest.Actions.CRUD.__response__(__MODULE__, unquote(operation), unquote(type), unquote(opts))
+    end
+  end
+
+  def __request__(mod, operation, type, opts) do
+    Module.put_attribute(mod, :riakc_operation_fields, {:request,operation, type})
+  end
+
+  def __response__(mod, operation, type, opts) do
+    Module.put_attribute(mod, :riakc_operation_fields, {:response,operation, type})
+  end
+
+  def __types__(fields) do
+    {request,response} = 
+      Enum.split_while(fields,fn({direction,_op,_type})->
+        direction == :request 
+      end)
+    quoted_request =
+      Enum.map(fields, fn {operation, type} ->
+        quote do
+          def __request__(unquote(operation)) do
+            unquote(Macro.escape(type))
+          end
+        end
+      end)
+
+    quoted_response =
+      Enum.map(fields, fn {operation, type} ->
+        quote do
+          def __response__(unquote(operation)) do
+            unquote(Macro.escape(type))
+          end
+        end
+      end)
+
+
+    quote do
+      unquote(quoted_request)
+      def  __request__(_), do: nil
+      unquote(quoted_response)
+      def  __response__(_), do: nil
+    end
+  end
+
 
 end
