09b6ffab0c2cf0710c5c57643f573e778def4bfa
David.Gao <david.alpha.fox@gmail.com>
2016-05-12 08:11:15 +0800

    Version Module may crash

--- lib/tools/version.ex
+++ lib/tools/version.ex
@@ -1,24 +1,45 @@
 defmodule RiakcCommon.Tools.Version do  
 
-  defp vsn(app) do
+  defp with_vsn(app,action) do
     Application.load(app)
     vsn = Application.spec(app,:vsn)
-    vsn = List.to_string(vsn)
-    String.split(vsn,".")
+    if nil == vsn do 
+      nil
+    else
+      vsn = List.to_string(vsn)
+      vsn = String.split(vsn,".")
+      action.(vsn)
+    end
   end
 
   def major(app) do
-    version = vsn(app)
-    :lists.nth(1,version)
+    with_vsn(app, fn(version)->
+      if length(version) >= 1 do
+        :lists.nth(1,version)
+      else 
+        nil
+      end
+    end)
   end
 
   def mirror(app) do
-    version = vsn(app)
-    :lists.nth(2,version)
+    with_vsn(app, fn(version)->
+      if length(version) >= 2 do
+        :lists.nth(2,version)
+      else
+        nil
+      end
+    end)
   end
+  
   def revision(app) do
-    version = vsn(app)
-    :lists.nth(3,version)
+    with_vsn(app, fn(version)->
+      if length(version) >= 3 do
+        :lists.nth(3,version)
+      else
+        nil
+      end
+    end)
   end
 
 end
